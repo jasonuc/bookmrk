03a1811fed11e5ed452127c916ea6e42332851cc
jasonuc <mrjasonu@gmail.com>
2024-07-16 13:08:11 +0100

    chore: Remove unused code in app.controller.ts and app.service.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ClerkAuthGuard } from './guards/clerk-auth.guard';
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
+  controllers: [],
   providers: [
     {
       provide: APP_GUARD,
       useClass: ClerkAuthGuard,
     },
-    AppService,
   ],
 })
 export class AppModule {}
