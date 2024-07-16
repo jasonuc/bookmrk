7761a5b1a94ccd799e0589e1a4d02f46ad5f042d
jasonuc <mrjasonu@gmail.com>
2024-07-16 09:52:38 +0100

    chore: Remove unused code in app.controller.ts and app.service.ts

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { clerkClient } from '@clerk/clerk-sdk-node';
 
 @Injectable()
-export class AppService {
-  async getUsers() {
-    return await clerkClient.users.getUserList();
-  }
-}
+export class AppService {}
