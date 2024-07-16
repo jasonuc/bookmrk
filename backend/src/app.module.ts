57223563701cb5292fe5ffff5709ec6092b23276
jasonuc <mrjasonu@gmail.com>
2024-07-16 13:20:39 +0100

    feat: Add Books module and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ClerkAuthGuard } from './guards/clerk-auth.guard';
+import { BooksModule } from '@/books/books.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), BooksModule],
   controllers: [],
   providers: [
     {
---
+++ backend/src/books/books.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { PrismaService } from '@/prisma/prisma.service';
+
+@Injectable()
+export class BooksService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  findAll() {
+    return `This action returns all books`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
