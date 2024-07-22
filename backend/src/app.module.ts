6e2a51d12b5aea1d3cf1473348fb4cbb9b705197
Jason <mrjasonu@gmail.com>
2024-07-22 03:48:10 +0100

    chore: Remove unused code and dependencies for users, clerk, and guards

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-// import { APP_GUARD } from '@nestjs/core';
-// import { ClerkAuthGuard } from './guards/clerk-auth.guard';
 import { BooksModule } from '@/books/books.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BooksModule, UsersModule],
+  imports: [ConfigModule.forRoot(), BooksModule],
   controllers: [],
-  providers: [
-    // {
-    //   provide: APP_GUARD,
-    //   useClass: ClerkAuthGuard,
-    // },
-  ],
+  providers: [],
 })
 export class AppModule {}
