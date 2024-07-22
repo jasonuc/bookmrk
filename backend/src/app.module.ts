571b0d380b910a0b28c546b0f4d713fc3a53a06e
Jason <mrjasonu@gmail.com>
2024-07-22 03:19:37 +0100

    feat: Add Users module and controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 // import { APP_GUARD } from '@nestjs/core';
 // import { ClerkAuthGuard } from './guards/clerk-auth.guard';
 import { BooksModule } from '@/books/books.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BooksModule],
+  imports: [ConfigModule.forRoot(), BooksModule, UsersModule],
   controllers: [],
   providers: [
     // {
