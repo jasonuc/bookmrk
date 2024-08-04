8184dc8e8fe62d4eac23895c745501b445a26782
Jason <mrjasonu@gmail.com>
2024-08-04 17:18:13 +0100

    feat: Add ActionButton component to books data table
    This commit adds the ActionButton component to the books data table, allowing users to perform various actions on each book entry. The ActionButton component includes options such as opening the book, viewing notes, writing notes, editing book details, and deleting the book. This enhancement improves the functionality and user experience of the books data table.

---
+++ frontend/src/components/books-data-table/action-button.tsx
@@ -0,0 +1,58 @@
+import { BookOpen, Eye, FolderOpen, MoreHorizontal, Pen, Trash2 } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Book } from "@/types/book.type"
+
+export default function ActionButton({ book }: { book: Book }) {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+                <DropdownMenuItem className="space-x-2">
+                    <FolderOpen className="w-4 h-4" />
+                    <p>Open</p>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="space-x-2">
+                    <Eye className="w-4 h-4" />
+                    <p>View notes</p>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem className="space-x-2">
+                    <Pen className="w-4 h-4" />
+                    <p>Write note</p>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="space-x-2">
+                    <BookOpen className="w-4 h-4" />
+                    <p>Edit book details</p>
+                </DropdownMenuItem>
+
+                <DropdownMenuSeparator />
+
+                <DropdownMenuItem className="space-x-2 !text-red-700">
+                    <Trash2 className="w-4 h-4" />
+                    <p>Delete book</p>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
--- frontend/src/components/books-data-table/columns.tsx
+++ frontend/src/components/books-data-table/columns.tsx
@@ -1,10 +1,11 @@
 "use client";
+
 import { ColumnDef } from "@tanstack/react-table";
 import { Book, Status } from "@/types/book.type";
 import { Rating } from '@smastrom/react-rating'
 import FormattedStatus from "@/components/formatted-status";
 import TableHeader from "./table-header";
-
+import ActionButton from "./action-button";
 
 export const booksColumns: ColumnDef<Book>[] = [
   {
@@ -25,7 +26,7 @@ export const booksColumns: ColumnDef<Book>[] = [
     cell: ({ row }) => {
       const rating = parseInt(row.getValue("rating"));
       return (<Rating
-        style={{ maxWidth: 80 }}
+        className="max-w-[80px]"
         value={rating}
         readOnly />)
     }
@@ -37,6 +38,17 @@ export const booksColumns: ColumnDef<Book>[] = [
       const lastUpdated = new Date(row.getValue("lastUpdated")).toDateString();
 
       return lastUpdated;
+    }
+  },
+  {
+    accessorKey: "action",
+    header: () => <TableHeader header="" />,
+    cell: ({ row }) => {
+      const book = row.original;
+
+      return (
+        <ActionButton book={book} />
+      )
     }
   }
 ]
