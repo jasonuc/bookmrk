50b059a497a4c4b489abe9984a5f8d06f2e99166
Jason <mrjasonu@gmail.com>
2024-08-04 15:41:42 +0100

    feat: adds more styling to the landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,25 @@
+import LetterPullup from "@/components/magicui/letter-pullup";
+import RetroGrid from "@/components/magicui/retro-grid";
+import ShinyButton from "@/components/magicui/shiny-button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-[100dvh] border">
-      <p>Hello World</p>
+    <main className="relative flex h-screen w-full flex-col items-center justify-center overflow-hidden rounded-lg border bg-background md:shadow-xl">
+
+      <div className="flex flex-col items-center justify-center space-y-2">
+        <h1 className="pointer-events-none z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-7xl font-bold leading-none tracking-tighter text-transparent">
+          Bookmrk
+        </h1>
+        <LetterPullup words="Read it, save it, log it !)"
+          className="text-sm" />
+      </div>
+
+      <Link href='/sign-up' className="mt-7">
+          <ShinyButton text="Begin your journey now 🎉" />
+      </Link>
+
+      <RetroGrid />
     </main>
   )
 }
