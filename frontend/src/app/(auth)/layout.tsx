ee4efe481ed2b2b95e07253c66b045f2da38b408
Jason <mrjasonu@gmail.com>
2024-08-05 14:28:21 +0100

    feat: adds retro grid background to auth pages

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,8 +1,11 @@
+import RetroGrid from "@/components/magicui/retro-grid";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className={"w-dvw h-dvh flex flex-col items-center justify-center"}>
       <main>
         {children}
+        <RetroGrid />
       </main>
     </div>
   )
