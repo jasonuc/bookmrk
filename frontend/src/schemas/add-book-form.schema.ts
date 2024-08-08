e143bb6c810f3a9515e531af20d43122e07db9a9
Jason <mrjasonu@gmail.com>
2024-08-08 12:36:27 +0100

    feat: Update addBookFormSchema to allow optional imageUrl
    This commit updates the addBookFormSchema in the add-book-form.schema.ts file to allow the imageUrl field to be optional. Previously, the field was required, but now it can be left empty if the user does not have an image URL for the book. This change improves the flexibility and usability of the add book form.

--- frontend/src/schemas/add-book-form.schema.ts
+++ frontend/src/schemas/add-book-form.schema.ts
@@ -3,7 +3,7 @@ import { Status } from "@/types/book.type";
 
 export const addBookFormSchema = z.object({
     title: z.string().trim().min(1, { message: "Required" }),
-    imageUrl: z.string().url().optional(),
+    imageUrl: z.string().url().or(z.string().optional()),
     rating: z.number().min(0).max(5).default(0),
     status: z.enum([Status.DNF, Status.FINISHED, Status.READING, Status.TBR]),
 });
