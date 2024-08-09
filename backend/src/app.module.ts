4844e204e90babacb41bfa79bf7930fdd94e121e
Jason <mrjasonu@gmail.com>
2024-08-09 19:17:50 +0100

    feat: Initialise notes resource

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { UsersModule } from './users/users.module';
 import { WebhooksModule } from './webhooks/webhooks.module';
 import { BooksModule } from './books/books.module';
 import { AppController } from './app.controller';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { AppController } from './app.controller';
     WebhooksModule,
     UsersModule,
     BooksModule,
+    NotesModule,
   ],
   controllers: [AppController],
   providers: [],
