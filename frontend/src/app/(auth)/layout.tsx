6236581881920db2178bcdd7335e4ca768681861
Jason <mrjasonu@gmail.com>
2024-08-11 07:59:03 +0100

    fix: fix Ui of sign in and sign up page

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative w-full h-full flex items-center justify-center">
+    <div className="relative w-full h-screen flex items-center justify-center">
       <div className="absolute top-5">
         <Link href="/">
           <h1 className="cursor-pointer z-10 whitespace-pre-wrap bg-gradient-to-b from-[#ffd319] via-[#ff2975] to-[#8c1eff] bg-clip-text text-center text-5xl font-bold leading-none tracking-tighter text-transparent">
