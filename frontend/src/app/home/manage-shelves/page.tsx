189d3f3981d3878168a811d02072956389949127
Jason <mrjasonu@gmail.com>
2024-08-12 17:16:27 +0100

    feat: Add Ripple component to ManageShelvesPage

--- frontend/src/app/home/manage-shelves/page.tsx
+++ frontend/src/app/home/manage-shelves/page.tsx
@@ -1,3 +1,4 @@
+import Ripple from "@/components/magicui/ripple";
 import ManageShelvesButtonSection from "@/components/manage-shelves-button-section";
 import ShelfDisplayCard from "@/components/shelf-display-card";
 import { Shelf } from "@/types/shelf.type";
@@ -21,18 +22,28 @@ async function getUserShelves() {
 
 export default async function ManageShelvesPage() {
 
-  const userShelves = await getUserShelves();
+  const userShelves = [] as Shelf[];
 
   return (
     <div className="pb-10 px-2 md:px-5">
       <ManageShelvesButtonSection />
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mx-auto max-w-6xl">
+
+      {userShelves.length === 0 && (
+        <div className="relative min-h-[700px] w-full flex flex-col items-center justify-center overflow-hidden">
+          <p className="z-10 whitespace-pre-wrap text-center text-5xl font-bold tracking-tighter text-black">
+            No Bookshelves
+          </p>
+          <Ripple />
+        </div>
+      )}
+
+      {userShelves.length !== 0 && (<div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 mx-auto max-w-6xl">
 
         {userShelves.map((shelf, key) => (
           <ShelfDisplayCard key={key} {...shelf} />
         ))}
 
-      </div>
+      </div>)}
     </div>
   )
 }
